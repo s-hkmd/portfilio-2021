03a166fec3e3080a789083ac2d7bba3dae3e3d0c
S.Hakamada <sho.hkmd@gmail.com>
2022-02-06 15:17:04 +0900

    Fix: Works Detail
    リファクタ

--- src/js/page-works-detail.js
+++ src/js/page-works-detail.js
@@ -4,26 +4,40 @@
  */
 
 /*
-  パララックスエフェクトなど
+  エフェクトまわり
 ---------------------------------------------------*/
+/**
+ * パララックスエフェクト
+ */
 const parallax = () => {
+  /**
+   * 変数・定数
+   */
+  const weight1 = 0.2;
+  const weight2 = 0.2;
+  const works_kv = document.querySelector('#js-works-kv');
+  const cover_image = document.querySelector('#js-works-cover-image');
+  const flow_text = document.querySelector('#js-flow-text');
+
+  /**
+   * イベントリスナー
+   */
   window.addEventListener('scroll', () => {
     const scrolled = document.documentElement.scrollTop;
-    const weight1 = 0.2;
-    const weight2 = 0.2;
-    
-    // パララックスエフェクト
-    const works_kv = document.querySelector('#js-works-kv');
-    if (works_kv) works_kv.style.transform = `translateY(${0 - scrolled * weight1 * 0.3}%)`;
 
-    const cover_image = document.querySelector('#js-works-cover-image');
+    // パララックスエフェクト
+    if (works_kv) works_kv.style.transform = `translateY(${0 - scrolled * weight1 * 0.3}%)`;
     if (cover_image) cover_image.style.transform = `translateY(calc(-20vh + ${scrolled * weight2 / 2}px))`;
 
     // デザインの画像のとこのデカめな文字
-    const flow_text = document.querySelector('#js-flow-text');
     if (flow_text) flow_text.setAttribute('style', `transform:translateX(${0 - scrolled * 0.005}%);transition: transform .3s ease-out;`);
   });
+};
 
+/**
+ * マウスホバーエフェクト
+ */
+const nextWorksMouseHover = () => {
   // Nextのとこ
   const next_works_image = document.querySelector('#js-next-thumbnail');
   if (next_works_image) {
@@ -62,4 +76,5 @@ const parallax = () => {
  */
 export const initWorksDetail = () => {
   parallax();
+  nextWorksMouseHover();
 };
