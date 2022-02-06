b5e4e8e25e78ceb35ce68b8ba526797a95fa0e9b
S.Hakamada <sho.hkmd@gmail.com>
2022-02-06 14:19:52 +0900

    Fix: Wokrs Detail

--- src/js/page-works-detail.js
+++ src/js/page-works-detail.js
@@ -14,40 +14,40 @@ const parallax = () => {
     
     // パララックスエフェクト
     const works_kv = document.querySelector('#js-works-kv');
-    if (works_kv) works_kv.style.transform = `transform: translateY(${0 - scrolled * weight1 * 0.3}%)`;
+    if (works_kv) works_kv.style.transform = `translateY(${0 - scrolled * weight1 * 0.3}%)`;
 
     const cover_image = document.querySelector('#js-works-cover-image');
     if (cover_image) cover_image.style.transform = `translateY(calc(-20vh + ${scrolled * weight2 / 2}px))`;
-  
+
     // デザインの画像のとこのデカめな文字
     const flow_text = document.querySelector('#js-flow-text');
     if (flow_text) flow_text.setAttribute('style', `transform:translateX(${0 - scrolled * 0.005}%);transition: transform .3s ease-out;`);
   });
-  
+
   // Nextのとこ
   const next_works_image = document.querySelector('#js-next-thumbnail');
   if (next_works_image) {
     next_works_image.onmousemove = function(event) {
       let mouseX, mouseY, offsetX = 0, offsetY = 0;
       let element = this;
-  
+
       while (element) {
         offsetX += element.offsetLeft;
         offsetY += element.offsetTop;
         element = element.offsetParent;
       }
-  
+
       if (event) {
-        mouseX = event.pageX - offsetX * 2.25;
-        mouseY = event.pageY - offsetY * 1.05;
+        mouseX = event.clientX - offsetX * 2.25;
+        mouseY = event.clientY - offsetY * 1.05;
       } else {
-        mouseX = event.pageX + document.body.scrollLeft - offsetX;
-        mouseY = event.pageY + document.body.scrollTop  - offsetY;
+        mouseX = event.clientX + document.body.scrollLeft - offsetX;
+        mouseY = event.clientY + document.body.scrollTop  - offsetY;
       }
-  
+
       next_works_image.setAttribute('style', `transform:translate(${mouseX * 0.1}px, ${mouseY * 0.1}px);`);
     };
-  
+
     next_works_image.onmouseleave = function() {
       next_works_image.removeAttribute('style');
     };
